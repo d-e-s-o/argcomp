93d19667a03be811bdb799f72856fe9eb2b5f247
Daniel Mueller <deso@posteo.net>
2017-01-23 06:24:19 -0800

    Add packaging functionality
    This change adds support for packaging and distributing the cleanup
    package using the setuptools package. Providing the means for packaging
    in terms of Python infrastructure compared to at the operating system
    distribution level has the advantages that
    a) we are independent of distributions and can even use the native
       Python way of distributing things and
    b) it simplifies creation of distribution specific packages because most
       distributions or package managers provide means to interface with
       setuptools packaged Python modules in a generic fashion.

---
+++ argcomp/setup.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+#/***************************************************************************
+# *   Copyright (C) 2017 Daniel Mueller (deso@posteo.net)                   *
+# *                                                                         *
+# *   This program is free software: you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation, either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+# ***************************************************************************/
+
+from os.path import (
+  dirname,
+  join,
+)
+from setuptools import (
+  setup,
+)
+
+
+def _importMeta():
+  """Import and return the packages's __meta__ module."""
+  # We must not import the package by normal means because if we have
+  # dependencies those might not be resolved at the time of execution of
+  # this script.
+  with open(join(dirname(__file__), "src", "deso", "argcomp", "__meta__.py")) as f:
+    globals_ = {}
+    locals_ = {}
+    exec(f.read(), globals_, locals_)
+    return locals_
+
+
+def retrieveName():
+  """Retrieve the package's name."""
+  return _importMeta()["name"]()
+
+
+def retrieveVersion():
+  """Retrieve the packages's version."""
+  return _importMeta()["version"]()
+
+
+def retrieveDescription():
+  """Retrieve a description of the package."""
+  return _importMeta()["description"]()
+
+
+setup(
+  name = retrieveName(),
+  author = "Daniel Mueller",
+  author_email = "deso@posteo.net",
+  maintainer = "Daniel Mueller",
+  maintainer_email = "deso@posteo.net",
+  version = retrieveVersion(),
+  description = retrieveDescription(),
+  url = "https://github.com/d-e-s-o/argcomp",
+  classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development :: User Interfaces",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: System :: Shells",
+  ],
+  keywords = "argcomp argparse ArgumentParser shell completion",
+  license = "GPLv3",
+  namespace_packages = [
+    "deso",
+  ],
+  packages = [
+    "deso.argcomp",
+    "deso.argcomp.test",
+  ],
+  package_dir = {
+    "deso.argcomp": join("src", "deso", "argcomp"),
+    "deso.argcomp.test": join("src", "deso", "argcomp", "test"),
+  },
+  test_suite = "deso.argcomp.test.allTests",
+  # We are not interested in egg-style distribution and it gets
+  # prevented by declaring the distribution as not zip safe.
+  zip_safe = False,
+)
---
+++ argcomp/src/deso/argcomp/__meta__.py
@@ -0,0 +1,35 @@
+# __meta__.py
+
+#/***************************************************************************
+# *   Copyright (C) 2017 Daniel Mueller (deso@posteo.net)                   *
+# *                                                                         *
+# *   This program is free software: you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation, either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+# ***************************************************************************/
+
+"""Information about the deso.argcomp package."""
+
+
+def name():
+  """Retrieve the name of the package."""
+  return "argcompd"
+
+
+def description():
+  """Retrieve a description of the package."""
+  return "A package providing automated shell completion support for argparse."
+
+
+def version():
+  """Retrieve the package's current version."""
+  return "0.9.0.0"
